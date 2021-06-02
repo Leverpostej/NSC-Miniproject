ca5b872546a1ed2af4bea68799442fe418c6f9e1
Adamsen5482 <tadams17@student.aau.dk>
2021-06-02 20:12:16 +0200

    pep8 fixes

--- f_GPU.py
+++ f_GPU.py
@@ -2,20 +2,17 @@ import pyopencl as cl
 from matplotlib import pyplot as plt
 import numpy as np
 
-%pylab inline
+# %pylab inline
 
-# Create the context (containing platform and device information) and command queue.
+# Create context (containing platform and device information) and command queue
 ctx = cl.create_some_context()
 devices = ctx.get_info(cl.context_info.DEVICES)
 print(devices)
 
 
 def mandelbrot_gpu(input):
-    
     queue = cl.CommandQueue(ctx)
-    
     output = np.zeros(input.shape).astype(np.int32)
-    
     # Source of the kernel itself.
     prg = cl.Program(ctx, """
     __kernel void mandelbrot(
@@ -35,22 +32,22 @@ def mandelbrot_gpu(input):
             }
             imag = 2* real*imag + input[gid].y;
             real = real2 - imag2 + input[gid].x;
-            
         }
     }
     """).build()
 
     mf = cl.mem_flags
-    input_opencl = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=input)
+    input_opencl = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                             hostbuf=input)
     output_opencl = cl.Buffer(ctx, mf.WRITE_ONLY, output.nbytes)
 
-
     Event = prg.mandelbrot(queue, output.shape, None, input_opencl,
-                   output_opencl)
+                           output_opencl)
     Event.wait()
     cl.enqueue_copy(queue, output, output_opencl)
-    
+
     return output
+
 
 def mandelbrot():
     width = 16000
@@ -59,13 +56,14 @@ def mandelbrot():
     ymin, ymax = -1.5, 1.5  # y range
     a = np.linspace(xmin, xmax, width).astype(np.float32)
     b = np.linspace(ymin, ymax, height).astype(np.float32)
-    x,y = np.meshgrid(a,b)
+    x, y = np.meshgrid(a, b)
     z = x + y * 1j
     z = z.flatten()
     o = mandelbrot_gpu(z)
     return (o)
 
-%time I = mandelbrot()
-I = np.reshape(I,(16000,16000))
-plt.imshow(I, cmap=plt.cm.hot)  # plot the image
+
+result = mandelbrot()
+result = np.reshape(result, (16000, 16000))
+plt.imshow(result, cmap=plt.cm.hot)  # plot the image
 plt.show()
