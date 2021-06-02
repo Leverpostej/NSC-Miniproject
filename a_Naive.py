57e7ec61cdd67e2aa1e195f118cc5dd6b2a2c982
Adamsen5482 <tadams17@student.aau.dk>
2021-06-02 22:32:37 +0200

    naive and numpy fixes
    pep8 adherence, saved output as image and h5, noted down run time

--- a_Naive.py
+++ a_Naive.py
@@ -1,45 +1,53 @@
 from PIL import Image, ImageDraw
+import h5py
+import time
 
 
-def mandelbrot(c, treshold, Max_iter=80):
+def mandelbrot(c, threshold, max_iter=80):
     z = 0
     n = 0
-    while abs(z) <= treshold and n < Max_iter:
+    while abs(z) <= threshold and n < max_iter:
         z = z*z + c
         n += 1
-    return n
+    return n, z
+
 
 def main():
-    MAX_ITER = 80
-    treshold = 2
+    max_iter = 80
+    threshold = 2
     # Image size (pixels)
-    WIDTH = 600
-    HEIGHT = 400
+    width = 600
+    height = 400
 
     # Plot window
-    RE_START = -2
-    RE_END = 1
-    IM_START = -1
-    IM_END = 1
+    re_start = -2
+    re_end = 1
+    im_start = -1
+    im_end = 1
 
-    palette = []
-
-    im = Image.new('HSV', (WIDTH, HEIGHT), (0, 0, 0))
+    im = Image.new('HSV', (width, height), (0, 0, 0))
     draw = ImageDraw.Draw(im)
-
-    for x in range(0, WIDTH):
-        for y in range(0, HEIGHT):
+    start = time.time()
+    z = []
+    for x in range(0, width):
+        for y in range(0, height):
             # Convert pixel coordinate to complex number
-            c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
-                        IM_START + (y / HEIGHT) * (IM_END - IM_START))
+            c = complex(re_start + (x / width) * (re_end - re_start),
+                        im_start + (y / height) * (im_end - im_start))
             # Compute the number of iterations
-            m = mandelbrot(c,treshold, MAX_ITER)
+            m, z2 = mandelbrot(c, threshold, max_iter)
             # The color depends on the number of iterations
-            hue = int(255 * m / MAX_ITER)
+            hue = int(255 * m / max_iter)
             saturation = 255
-            value = 255 if m < MAX_ITER else 0
+            value = 255 if m < max_iter else 0
             # Plot the point
             draw.point([x, y], (hue, saturation, value))
-
+            z.append(z2)
+    h5f = h5py.File('naive.h5', 'w')
+    h5f.create_dataset('dataset_1', data=z)
+    h5f.close()
+    print("computation time", time.time()-start)
     im.convert('RGB').save('output.png', 'PNG')
 
+
+main()
--- b_Numpy.py
+++ b_Numpy.py
@@ -1,28 +1,36 @@
 import numpy as np
-from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
+import h5py
+import time
 
 
-def mandelbrot_set(c, treshold, num_iterations):
-    #determine for each point c of a part of the complex plane, whether z_n is bounded
+def mandelbrot_set(c, threshold, num_iterations):
+    # determine for each point c of a part of the complex plane, whether z_n is bounded
 
-
-
-    z = 0 # complex number, real part represented by a displacement along the x-axis, imaginary part by a displacement along the y-axis
-
+    # complex number, real part represented by a displacement along the x-axis
+    # imaginary part by a displacement along the y-axis
+    z = 0
     for g in range(num_iterations):
         z = z ** 2 + c
-    n = abs(z) < treshold
-    return n
+    n = np.abs(z) < threshold
+    return n, z
 
 
-num_iterations=500
-threshold = 2
+def main():
+    num_iterations = 500
+    threshold = 2
+    start = time.time()
+    x, y = np.ogrid[-2:1:5000j, -1.5:1.5:5000j]
+    c = x + 1j * y
+    n, z = mandelbrot_set(c, threshold, num_iterations)
+    print("computation time", time.time()-start)
+    plt.imshow(n.T, extent=[-2, 1, -1.5, 1.5], cmap=plt.cm.get_cmap("hot"), aspect='auto')
 
-x,y=np.ogrid[-2:1:5000j,-1.5:1.5:5000j]
-c=x + 1j*y
-n = mandelbrot_set(c, threshold, num_iterations)
+    plt.show()
+    plt.savefig('numpy.png')
+    h5f = h5py.File('numpy.h5', 'w')
+    h5f.create_dataset('dataset_1', data=z)
+    h5f.close()
 
-plt.imshow(n.T,extent=[-2,1,-1.5,1.5],cmap=plt.cm.get_cmap("hot"), aspect='auto')
 
-plt.show()
+main()
